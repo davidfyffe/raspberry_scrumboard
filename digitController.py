68a7d496d742a677802cbc31113cdc1786c8fbdf
davidfyffe <davidfyffe@yahoo.co.uk>
2016-10-28 19:05:39 +0100

    update inputs

--- digitController.py
+++ digitController.py
@@ -12,12 +12,12 @@ class Shifter():
     def __init__(self):
         self.setupBoard()
         self.pause=0
-        
+
     def tick(self):
         gpio.output(Shifter.clock,gpio.HIGH)
         sleep(self.pause)
         gpio.output(Shifter.clock,gpio.LOW)
-        sleep(self.pause)		
+        sleep(self.pause)
 
     def setValue(self,value):
         for i in range(24):
@@ -38,7 +38,7 @@ class Shifter():
 
         gpio.setmode(gpio.BCM)
 
-        #gpio.setup(Shifter.inputA,gpio.OUT) 
+        #gpio.setup(Shifter.inputA,gpio.OUT)
         #gpio.output(Shifter.inputA,gpio.HIGH)
 
         gpio.setup(Shifter.inputB,gpio.OUT)
@@ -51,24 +51,24 @@ class Shifter():
         gpio.output(Shifter.clearPin,gpio.HIGH)
 
 
-def main():
-    pause=0.5
-    #gpio.setmode(gpio.BOARD)
-    shifter=Shifter()
-    running=True
-    while running==True:
-        try:
-        #shifter.clear()
-        #shifter.setValue(1)
-        #sleep(1)
-            shifter.clear()
-            #shifter.setValue(0x0AAAAAA)
-            sleep(pause)
-            #shifter.clear()
-            shifter.setValue(0x0555555)
-            sleep(pause)
-        except KeyboardInterrupt:
-            running=False
-
-if __name__ == '__main__':
-	main()
+# def main():
+#     pause=0.5
+#     #gpio.setmode(gpio.BOARD)
+#     shifter=Shifter()
+#     running=True
+#     while running==True:
+#         try:
+#         #shifter.clear()
+#         #shifter.setValue(1)
+#         #sleep(1)
+#             shifter.clear()
+#             #shifter.setValue(0x0AAAAAA)
+#             sleep(pause)
+#             #shifter.clear()
+#             shifter.setValue(0x0555555)
+#             sleep(pause)
+#         except KeyboardInterrupt:
+#             running=False
+#
+# if __name__ == '__main__':
+# 	main()
